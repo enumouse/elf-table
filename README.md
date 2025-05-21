3bc8905acaa37d7d15baefe5cf5a59bd80ecab38
Calamari <90973461+enumouse@users.noreply.github.com>
2025-05-21 16:48:50 +0100

    Working project commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Elf-Table
+A super simple elf header viewer.
+
+![screenshot](.github/screenshot.png)
+
+## Usage
+```
+elf-table path/to/file.elf
+```
+
+## Install
+```
+cargo install --git https://github.com/enumouse/elf-table elf-table
+```
+Binaries may be available in the releases tab
+
+## Build
+```
+git clone https://github.com/enumouse/elf-table
+cd elf-table
+cargo build --release
+```
+
+## Contributing
+This project is absolutely tiny and basic. If you want to add features, you may be better of starting a new project, with or without this as a base, instead of adding it here. This was created in a handful of minutes purely as a 'I want this and idk if it exists' project and nothing more.
+
+Regardless, I will accept basic pull requests, but please keep the above in mind.
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use elf::ElfBytes;
 use table::Table;
 
 mod table;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let table = Table::new();
+    let mut table = Table::new();
     
     // Check we have parameters
     if args.len() < 2 {
@@ -27,5 +28,35 @@ fn main() {
         .expect("Unable to read file");
 
     // Fetch Header Data
+    let elf = ElfBytes::<elf::endian::AnyEndian>::minimal_parse(&contents)
+        .expect("Failed to parse ELF");
+    let header = elf.ehdr;
 
+    // Add rows
+    let rows = vec![
+        ("Class", format!("{:?}", header.class)),
+        ("Version", format!("{:?}", header.version)),
+        ("OS ABI", format!("{:?}", header.osabi)),
+        ("ABI Version", format!("{}", header.abiversion)),
+        ("Type", format!("{:?}", header.e_type)),
+        ("Machine", format!("{:?}", header.e_machine)),
+        ("Version (Hex)", format!("{:#x}", header.version)),
+        ("Entry Point", format!("{:#x}", header.e_entry)),
+        ("Program Header Offset", format!("{}", header.e_phoff)),
+        ("Section Header Offset", format!("{}", header.e_shoff)),
+        ("Flags", format!("{:#x}", header.e_flags)),
+        ("Header Size", format!("{}", header.e_ehsize)),
+        ("Program Header Entry Size", format!("{}", header.e_phentsize)),
+        ("Program Header Count", format!("{}", header.e_phnum)),
+        ("Section Header Entry Size", format!("{}", header.e_shentsize)),
+        ("Section Header Count", format!("{}", header.e_shnum)),
+        ("Section Header String Index", format!("{}", header.e_shstrndx)),
+    ];
+
+    for (title, value) in rows {
+        table.add_row(title, &value);
+    }
+
+    // Draw Table
+    table.draw();
 }
